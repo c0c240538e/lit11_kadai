d657c7902bb8877c9fe129bb663cfdf7422ad499
c0c240538e <c0c240538e@edu.teu.ac.jp>
2024-06-14 17:54:21 +0900

    first test

--- live_share.py
+++ live_share.py
@@ -3,4 +3,6 @@ b=("c0c24017") #c0c24017
 c=int((a)[-3:]) #c0c24053
 d=int((b)[-3:]) #c0c24017
 ans = c + d #c0c24053
+ans2 = c * d
 print(str(c)+"と"+str(d) + "を足すと" + str(ans)+"になります") #c0c24017
+print(str(c)+"と"+str(d) + "をかけると" + str(ans2)+"になります") 
